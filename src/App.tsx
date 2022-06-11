0d876ca9d7940e108aa0963fa8dfb43f6848bbcf
RadyaBen <lizoguben.radya@gmail.com>
2022-06-11 22:50:11 +0300

    Create a reset button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 
 import { HeaderWithCounter } from './components/HeaderWithCounter';
 import { Battlefield } from './components/Battlefield';
+import { ResetButton } from './components/ResetButton';
 
 function App() {
+	const reset = () => console.log('reset');
+	
 	return (
 		<div>
 			<HeaderWithCounter turn={0} />
 			<Battlefield />
+			<ResetButton reset={reset}/>
 		</div>
 	);
 }
